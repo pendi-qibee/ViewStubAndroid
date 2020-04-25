6b47ed859cc8885eb1b665ca59195266e3bd2a50
pendi-qibee <pendimadyana@gmail.com>
2020-04-25 22:49:13 +0700

    Initial commit

---
+++ app/src/main/java/id/qibee/viewstub/ui/dashboard/DashboardFragment.kt
@@ -0,0 +1,43 @@
+package id.qibee.viewstub.ui.dashboard
+
+import android.os.Bundle
+import android.os.Handler
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import id.qibee.viewstub.R
+import kotlinx.android.synthetic.main.fragment_dashboard.*
+import java.util.*
+import kotlin.concurrent.schedule
+
+class DashboardFragment : Fragment() {
+
+    private lateinit var dashboardViewModel: DashboardViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        dashboardViewModel =
+                ViewModelProviders.of(this).get(DashboardViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
+        val textView: TextView = root.findViewById(R.id.text_dashboard)
+        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+
+        showImage()
+        return root
+    }
+
+    private fun showImage() {
+        Timer().schedule(1200){
+            viewStubFlameShot?.inflate()
+        }
+    }
+}
